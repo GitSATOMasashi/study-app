33e204f0a0245cff443d43ecdd5d89ebccd55c7d
GitSATOMasashi <sakata28.04@gmail.com>
2026-01-09 00:20:34 +0900

    あ

--- src/App.tsx
+++ src/App.tsx
@@ -55,36 +55,38 @@ function App() {
 
   return (
     <>
-      {records.length === 0 ? <div>Loading...</div> : (
+      {records.length === 0 ? (
+        <div>Loading...</div>
+      ) : (
         <div>
-        <h1>学習記録アプリ</h1>
-        <div>
-          <label>学習内容</label>
-          <input
-            onChange={(e) => setLearningContent(e.target.value)}
-            value={learningContent}
-            type="text"
-          />
+          <h1>学習記録アプリ！！</h1>
+          <div>
+            <label>学習内容</label>
+            <input
+              onChange={(e) => setLearningContent(e.target.value)}
+              value={learningContent}
+              type="text"
+            />
+          </div>
+          <div>
+            <label>学習時間</label>
+            <input
+              onChange={(e) => setLearningTime(Number(e.target.value))}
+              value={learningTime}
+              type="number"
+            />
+          </div>
+          <button onClick={handleAddRecord}>登録</button>
+          <div>{isError}</div>
+          <ul>
+            {records.map((record) => (
+              <li key={record.title}>
+                {record.id} - {record.title} - {record.time}時間
+              </li>
+            ))}
+          </ul>
+          <div>合計学習時間: {sumTime}時間</div>
         </div>
-        <div>
-          <label>学習時間</label>
-          <input
-            onChange={(e) => setLearningTime(Number(e.target.value))}
-            value={learningTime}
-            type="number"
-          />
-        </div>
-        <button onClick={handleAddRecord}>登録</button>
-        <div>{isError}</div>
-        <ul>
-          {records.map((record) => (
-            <li key={record.title}>
-              {record.id} - {record.title} - {record.time}時間
-            </li>
-          ))}
-        </ul>
-        <div>合計学習時間: {sumTime}時間</div>
-      </div>
       )}
     </>
   );
